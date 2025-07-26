4d5ecdb15ab95334ceb3f1337c1e9e6893245459
abdullah <outlook.tazriyan@gmail.com>
2025-07-26 10:46:01 +0600

    Added campaigns Read and post

--- index.js
+++ index.js
@@ -24,19 +24,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const campaignDB = client.db('CampaignDB')
+        const campaignCollection = campaignDB.collection('allCampaign')
+
+
+        app.get('/', (req, res) => {
+            res.send('CROWDCUBE database is running')
+        })
+        app.get('/campaigns', async (req, res) => {
+            const result = await campaignCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/campaigns', async (req, res) => {
+            const campaignInfo = req.body;
+            const result = await campaignCollection.insertOne(campaignInfo)
+
+            res.send(result)
+        })
+
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
-app.get('/', (req, res) => {
-    res.send('CROWDCUBE database is running')
-})
 app.listen(port)
